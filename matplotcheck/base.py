32bd338d1d63cab91ab636ad9303e8ea0cdb1290
Leah Wasser <leah.wasser@colorado.edu>
2018-12-27 16:27:18 -0700

    filling out plot type tests

--- matplotcheck/base.py
+++ matplotcheck/base.py
@@ -68,7 +68,8 @@ class PlotTester(object):
         Parameters
         ----------
         plot_type: string
-            String specifying the expected plot type. Options: 'scatter','bar', 'line'
+            String specifying the expected plot type. Options:
+            `scatter`,`bar`, `line`
         """
         if plot_type:
             if plot_type == "scatter":
@@ -85,7 +86,7 @@ class PlotTester(object):
                 )
             else:
                 raise ValueError(
-                    "plot_type must be on of the following string ['scatter', 'bar' or 'line']"
+                    "Plot_type to test must be either: scatter, bar or line"
                 )
 
     ## TITLES TESTS/HELPER FUNCTIONS ##
