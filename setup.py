774da81b9820f2dced2215896de16c1bbe854d17
Leah Wasser <leah.wasser@colorado.edu>
2019-03-27 10:30:03 -0600

    black and bumpversion  (#21)
    pypi has not been connected yet
    Docs are still not building but continuing with core setup.

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-import os
 from os import path
-# from numpy.distutils.core import setup
 from setuptools import setup
 
 
@@ -8,44 +6,30 @@ DISTNAME = "matplotcheck"
 DESCRIPTION = "Functions to check Matplotlib plot outputs"
 MAINTAINER = "Leah Wasser"
 MAINTAINER_EMAIL = "leah.wasser@colorado.edu"
-VERSION = "0.0.26"
-
-def configuration(parent_package="", top_path=None):
-    if os.path.exists("MANIFEST"):
-        os.remove("MANIFEST")
-
-    from numpy.distutils.misc_util import Configuration
-
-    config = Configuration(None, parent_package, top_path)
-    config.add_subpackage("matplotcheck")
-
-    return config
 
 
+# read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
 
+
 if __name__ == "__main__":
     setup(
-        configuration=configuration,
         name=DISTNAME,
         maintainer=MAINTAINER,
         maintainer_email=MAINTAINER_EMAIL,
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
-        url="https://github.com/earthlap/matplotcheck",
-        version=VERSION,
+        version="0.0.5",
+        packages=["matplotcheck"],
         install_requires=[
-            "pandas",
-            "numpy",
+            "numpy>=1.14.0",
             "folium",
             "geopandas",
-            "matplotlib",
+            "matplotlib>=2.0.0",
             "rasterio",
-            "python-dateutil",
-            "scipy",
         ],
         zip_safe=False,  # the package can run out of an .egg file
         classifiers=[
