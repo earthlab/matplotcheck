889c825cd4dc99e5b6378e2d665e27830d827a8f
Leah Wasser <leah.wasser@colorado.edu>
2019-04-02 18:11:05 -0600

    Bump version: 0.0.6 → 0.0.7

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = "2018, Leah Wasser, Kristin Curry"
 author = "Leah Wasser, Kristin Curry"
 
 # The short X.Y version
-version = "0.0.6"
+version = "0.0.7"
 # The full version, including alpha/beta/rc tags
-release = "0.0.6"
+release = "0.0.7"
 
 
 # -- General configuration ---------------------------------------------------
