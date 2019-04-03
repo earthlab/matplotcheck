b95fde019cffb2983c1a53e6d68e6b60683f37c3
Leah Wasser <leah.wasser@colorado.edu>
2019-04-02 18:38:52 -0600

    bump back

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
-        version="0.0.7",
+        version="0.0.6",
         packages=["matplotcheck"],
         install_requires=[
             "numpy>=1.14.0",
