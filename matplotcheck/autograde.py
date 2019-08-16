fd941e6f7f3dd254749a8fda656d1cedd8dd5230
jlpalomino <jenny.palomino@colorado.edu>
2019-08-16 11:37:53 -0600

    Run black on file

---
+++ matplotcheck/autograde.py
@@ -0,0 +1,89 @@
+# Wrapper functions for matplotcheck functions that run
+# and track the passing status of test functions and
+# provide formatted results of tests
+
+
+def run_test(
+    func,
+    points,
+    *args,
+    correct_message="default correct",
+    error_message="default error",
+    **kwargs
+):
+    """Runs a pre-defined test function and creates a dictionary
+    containing the results of the test
+
+    Parameters
+    ---------
+    func: pre-defined test function to be run
+    points: number of points assigned for passing test
+    args: arguments for test function
+    correct_message: custom message returned with passing test
+    error_message: custom message returned with failing test
+    kwargs: keyword arguments for test function
+
+    Returns
+    -------
+    results: dictionary with the following key:value pairs
+        points: number of points assigned based on test results
+        pass: passing status of test [True, False]
+        description: name of test function that was run
+        message: custom message returned based on passing status
+            [correct_message or error_message]
+        traceback: error message from test function (when pass is False)
+    """
+    results = {"points": 0, "pass": False}
+    score = 0
+    try:
+        fname = func.__name__
+        results["description"] = fname
+        func(*args, **kwargs)
+    except Exception as e:
+        results["message"] = error_message
+        results["traceback"] = e
+        pass
+    else:
+        results["pass"] = True
+        results["message"] = correct_message
+        results["points"] = points
+
+    return results
+
+
+def output_results(results):
+    """Prints a formatted message containing the total number of points
+    summed across a dictionary of results (from one or more tests)
+
+    Parameters
+    ---------
+    results: dictionary with the following key:value pairs
+        points: number of points assigned based on test results
+        pass: passing status of test [True, False]
+        description: name of test function that was run
+        message: custom message returned based on passing status
+            [correct_message or error_message]
+        traceback: error message from test function (when pass is False)
+
+    Returns
+    -------
+    points: total number of points summed across points in results dictionary
+    """
+    points = 0
+    for r in results:
+        points += r["points"]
+        print("Results for test '{}':".format(r["description"]))
+        if r["pass"]:
+            print(
+                " Pass! {msg} ({p} points)".format(
+                    msg=r["message"], p=r["points"]
+                )
+            )
+        else:
+            print(
+                " Fail! {msg} ({p} points)".format(
+                    msg=r["message"], p=r["points"]
+                )
+            )
+            print(" Traceback: {t}".format(t=r["traceback"]))
+    return points
