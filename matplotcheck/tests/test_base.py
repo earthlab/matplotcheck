dbc354bc64530534aba668efd5121cee941965e3
Nathan Korinek <nako1890@colorado.edu>
2020-02-13 17:40:45 -0700

    Format files to be flake8 acceptable (#197)
    * autograde flake8 fixes
    
    * Fixed base to be flake8 compliant (phew)
    
    * Made folium.py flake8 compliant
    
    * made notebook.py flake8 compliant
    
    * Fixed raster.py for flake8
    
    * Fixed timeseries.py
    
    * fixed conftest.py
    
    * fixed test_base.py

--- matplotcheck/tests/test_base.py
+++ matplotcheck/tests/test_base.py
@@ -44,8 +44,8 @@ def test_options(pt_line_plt):
     provided."""
 
     with pytest.raises(
-        ValueError,
-        match="Plot_type to test must be either: scatter, bar or line",
+            ValueError,
+            match="Plot_type to test must be either: scatter, bar or line",
     ):
         pt_line_plt.assert_plot_type("foo")
     plt.close()
@@ -55,7 +55,8 @@ def test_options(pt_line_plt):
 
 
 def test_assert_string_contains(pt_line_plt):
-    """Tests that assert_string_contains passes with correct expected strings."""
+    """Tests that assert_string_contains passes with correct expected
+    strings."""
     test_string = "this is a test string"
     string_expected = ["this", "is", "a", "test"]
     pt_line_plt.assert_string_contains(test_string, string_expected)
@@ -63,18 +64,21 @@ def test_assert_string_contains(pt_line_plt):
 
 
 def test_assert_string_contains_fails(pt_line_plt):
-    """Tests that assert_string_contains fails with incorrect expected strings."""
+    """Tests that assert_string_contains fails with incorrect expected
+    strings."""
     test_string = "this is a test string"
     string_expected = ["this", "is", "not", "a", "test"]
     with pytest.raises(
-        AssertionError, match="String does not contain expected string: not"
+            AssertionError,
+            match="String does not contain expected string: not"
     ):
         pt_line_plt.assert_string_contains(test_string, string_expected)
     plt.close()
 
 
 def test_assert_string_contains_or(pt_line_plt):
-    """Tests that assert_string_contains correctly passes when using OR logic."""
+    """Tests that assert_string_contains correctly passes when using OR
+    logic."""
     test_string = "this is a test string"
     string_expected = ["this", ["is", "not"], "a", "test"]
     pt_line_plt.assert_string_contains(test_string, string_expected)
@@ -82,11 +86,12 @@ def test_assert_string_contains_or(pt_line_plt):
 
 
 def test_assert_string_contains_or_fails(pt_line_plt):
-    """Tests that assert_string_contains correctly fails when using OR logic."""
+    """Tests that assert_string_contains correctly fails when using OR
+    logic."""
     test_string = "this is a test string"
     string_expected = ["this", "is", ["not", "jambalaya"], "a", "test"]
     with pytest.raises(
-        AssertionError, match="String does not contain at least one of: "
+            AssertionError, match="String does not contain at least one of: "
     ):
         pt_line_plt.assert_string_contains(test_string, string_expected)
     plt.close()
@@ -107,14 +112,16 @@ def test_assert_string_contains_handles_short_list_fails(pt_line_plt):
     test_string = "this is a test string"
     string_expected = [["this"], ["is"], ["not"]]
     with pytest.raises(
-        AssertionError, match="String does not contain expected string: not"
+            AssertionError,
+            match="String does not contain expected string: not"
     ):
         pt_line_plt.assert_string_contains(test_string, string_expected)
     plt.close()
 
 
 def test_assert_string_contains_passes_with_none(pt_line_plt):
-    """Tests that assert_string_contains passes when strings_expected is None"""
+    """Tests that assert_string_contains passes when strings_expected is
+    None"""
     test_string = "this is a test string"
     string_expected = None
     pt_line_plt.assert_string_contains(test_string, string_expected)
@@ -122,7 +129,8 @@ def test_assert_string_contains_passes_with_none(pt_line_plt):
 
 
 def test_assert_string_contains_passes_with_empty(pt_line_plt):
-    """Tests that assert_string_contains passes when strings_expected is empty"""
+    """Tests that assert_string_contains passes when strings_expected is
+    empty"""
     test_string = "this is a test string"
     string_expected = []
     pt_line_plt.assert_string_contains(test_string, string_expected)
