61344b06c7e5951f4a5afb5c65a54cd7b9da8a74
Nathan Korinek <nako1890@colorado.edu>
2020-02-13 18:03:54 -0700

    more black changes

--- matplotcheck/base.py
+++ matplotcheck/base.py
@@ -1139,9 +1139,8 @@ class PlotTester(object):
                 self.assert_line(
                     slope_exp,
                     intercept_exp,
-                    message_no_line="{0} line is not displayed properly".format(
-                        line_type
-                    ),
+                    message_no_line="{0} line is not "
+                    + "displayed properly".format(line_type),
                     message_data="{0} line does not cover dataset".format(
                         line_type
                     ),
