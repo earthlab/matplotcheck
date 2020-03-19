f48961394422fa9a36581e26de5561b7321a5fff
nkorinek <nathan.korinek@colorado.edu>
2020-03-19 14:16:51 -0600

    Broke tests up into individual files

---
+++ matplotcheck/tests/test_lines.py
@@ -0,0 +1,187 @@
+"""Tests for the vector module"""
+import pytest
+import matplotlib.pyplot as plt
+import geopandas as gpd
+from shapely.geometry import LineString
+from matplotcheck.vector import VectorTester
+import matplotlib
+
+matplotlib.use("Agg")
+
+
+@pytest.fixture
+def two_line_gdf():
+    """ Create Line Objects For Testing """
+    linea = LineString([(1, 1), (2, 2), (3, 2), (5, 3)])
+    lineb = LineString([(3, 4), (5, 7), (12, 2), (10, 5), (9, 7.5)])
+    gdf = gpd.GeoDataFrame([1, 2], geometry=[linea, lineb], crs="epsg:4326")
+    return gdf
+
+
+@pytest.fixture
+def multi_line_gdf(two_line_gdf):
+    """ Create a multi-line GeoDataFrame.
+    This has one multi line and another regular line.
+    """
+    # Create a single and multi line object
+    multiline_feat = two_line_gdf.unary_union
+    linec = LineString([(2, 1), (3, 1), (4, 1), (5, 2)])
+    out_df = gpd.GeoDataFrame(
+        geometry=gpd.GeoSeries([multiline_feat, linec]), crs="epsg:4326",
+    )
+    out_df["attr"] = ["road", "stream"]
+    return out_df
+
+
+@pytest.fixture
+def mixed_type_geo_plot(pd_gdf, multi_line_gdf):
+    """Create a point plot for testing"""
+    _, ax = plt.subplots()
+
+    pd_gdf.plot(ax=ax)
+    multi_line_gdf.plot(ax=ax)
+
+    axis = plt.gca()
+
+    return VectorTester(axis)
+
+
+@pytest.fixture
+def poly_line_plot(two_line_gdf):
+    """Create a line vector tester object."""
+    _, ax = plt.subplots()
+
+    two_line_gdf.plot(ax=ax)
+
+    axis = plt.gca()
+
+    return VectorTester(axis)
+
+
+@pytest.fixture
+def poly_multiline_plot(multi_line_gdf):
+    """Create a multiline vector tester object."""
+    _, ax = plt.subplots()
+
+    multi_line_gdf.plot(ax=ax, column="attr")
+
+    axis = plt.gca()
+
+    return VectorTester(axis)
+
+
+@pytest.fixture
+def poly_multiline_plot_bad(multi_line_gdf):
+    """Create a multiline vector tester object."""
+    _, ax = plt.subplots()
+
+    multi_line_gdf.plot(ax=ax)
+
+    axis = plt.gca()
+
+    return VectorTester(axis)
+
+
+def test_assert_line_geo(poly_line_plot, two_line_gdf):
+    """Test that lines are asserted correctly"""
+    poly_line_plot.assert_lines(two_line_gdf)
+    plt.close('all')
+
+
+def test_assert_multiline_geo(poly_multiline_plot, multi_line_gdf):
+    """Test that multi lines are asserted correctly"""
+    poly_multiline_plot.assert_lines(multi_line_gdf)
+    plt.close('all')
+
+
+def test_assert_line_geo_fail(poly_line_plot, multi_line_gdf):
+    """Test that lines fail correctly"""
+    with pytest.raises(AssertionError, match="Incorrect Line Data"):
+        poly_line_plot.assert_lines(multi_line_gdf)
+        plt.close('all')
+
+
+def test_assert_multiline_geo_fail(poly_multiline_plot, two_line_gdf):
+    """Test that multi lines fail correctly"""
+    with pytest.raises(AssertionError, match="Incorrect Line Data"):
+        poly_multiline_plot.assert_lines(two_line_gdf)
+        plt.close('all')
+
+
+def test_assert_line_fails_list(poly_line_plot):
+    """Test that assert_lines fails when passed a list"""
+    linelist = [
+        [(1, 1), (2, 2), (3, 2), (5, 3)],
+        [(3, 4), (5, 7), (12, 2), (10, 5), (9, 7.5)],
+    ]
+    with pytest.raises(ValueError, match="lines_expected is not expected ty"):
+        poly_line_plot.assert_lines(linelist)
+        plt.close('all')
+
+
+def test_assert_line_geo_passed_nothing(poly_line_plot):
+    """Test that assertion passes when passed None"""
+    poly_line_plot.assert_lines(None)
+    plt.close('all')
+
+
+def test_get_lines_geometry(poly_line_plot):
+    """Test that get_lines returns the proper values"""
+    lines = [(LineString(i[0])) for i in poly_line_plot.get_lines().values]
+    geometries = gpd.GeoDataFrame(geometry=lines)
+    poly_line_plot.assert_lines(geometries)
+    plt.close('all')
+
+
+def test_assert_lines_grouped_by_type(poly_multiline_plot, multi_line_gdf):
+    """Test that assert works for grouped line plots"""
+    poly_multiline_plot.assert_lines_grouped_by_type(multi_line_gdf, "attr")
+    plt.close('all')
+
+
+def test_assert_lines_grouped_by_type_fail(
+    poly_multiline_plot_bad, multi_line_gdf
+):
+    """Test that assert fails for incorrectly grouped line plots"""
+    with pytest.raises(AssertionError, match="Line attributes not accurate "):
+        poly_multiline_plot_bad.assert_lines_grouped_by_type(
+            multi_line_gdf, "attr"
+        )
+        plt.close('all')
+
+
+def test_assert_lines_grouped_by_type_passes_with_none(poly_multiline_plot):
+    """Test that assert passes if nothing is passed into it"""
+    poly_multiline_plot.assert_lines_grouped_by_type(None, None)
+    plt.close('all')
+
+
+def test_assert_lines_grouped_by_type_fails_non_gdf(
+    poly_multiline_plot, multi_line_gdf
+):
+    """Test that assert fails if a list is passed into it"""
+    with pytest.raises(ValueError, match="lines_expected is not of expected "):
+        poly_multiline_plot.assert_lines_grouped_by_type(
+            multi_line_gdf.to_numpy(), "attr"
+        )
+        plt.close('all')
+
+
+def test_mixed_type_passes(mixed_type_geo_plot, pd_gdf):
+    """Tests that points passes with a mixed type plot"""
+    mixed_type_geo_plot.assert_points(pd_gdf)
+    plt.close('all')
+
+
+def test_get_lines_by_collection(poly_multiline_plot):
+    """Test that get_lines_by_collection returns the correct values"""
+    lines_list = [
+        [
+            [(1, 1), (2, 2), (3, 2), (5, 3)],
+            [(3, 4), (5, 7), (12, 2), (10, 5), (9, 7.5)],
+            [(2, 1), (3, 1), (4, 1), (5, 2)],
+        ]
+    ]
+    sorted_lines_list = sorted([sorted(l) for l in lines_list])
+    assert sorted_lines_list == poly_multiline_plot.get_lines_by_collection()
+    plt.close('all')
---
+++ matplotcheck/tests/test_points.py
@@ -0,0 +1,135 @@
+"""Tests for the vector module"""
+import pytest
+import matplotlib.pyplot as plt
+import geopandas as gpd
+from matplotcheck.vector import VectorTester
+import matplotlib
+
+matplotlib.use("Agg")
+
+
+@pytest.fixture
+def bad_pd_gdf(pd_gdf):
+    """Create a point geodataframe with slightly wrong values for testing"""
+    return gpd.GeoDataFrame(
+        geometry=gpd.points_from_xy(
+            pd_gdf.geometry.x + 1, pd_gdf.geometry.y + 1
+        )
+    )
+
+
+@pytest.fixture
+def pt_geo_plot(pd_gdf):
+    """Create a geo plot for testing"""
+    _, ax = plt.subplots()
+    size = 0
+    point_symb = {"Tree": "green", "Bush": "brown"}
+
+    for ctype, points in pd_gdf.groupby("attr"):
+        color = point_symb[ctype]
+        label = ctype
+        size += 100
+        points.plot(color=color, ax=ax, label=label, markersize=size)
+
+    ax.legend(title="Legend", loc=(1.1, 0.1))
+
+    axis = plt.gca()
+
+    return VectorTester(axis)
+
+
+@pytest.fixture
+def pt_geo_plot_bad(pd_gdf):
+    """Create a geo plot for testing"""
+    _, ax = plt.subplots()
+
+    pd_gdf.plot(ax=ax)
+
+    axis = plt.gca()
+
+    return VectorTester(axis)
+
+
+@pytest.fixture
+def point_geo_plot(pd_gdf):
+    """Create a point plot for testing"""
+    _, ax = plt.subplots()
+
+    pd_gdf.plot(ax=ax)
+
+    axis = plt.gca()
+
+    return VectorTester(axis)
+
+
+def test_points_sorted_by_markersize_pass(pt_geo_plot, pd_gdf):
+    """Test points sorted by size of attribute pass"""
+    pt_geo_plot.assert_collection_sorted_by_markersize(pd_gdf, "attr")
+    plt.close('all')
+
+
+def test_points_sorted_by_markersize_fail(pt_geo_plot_bad, pd_gdf):
+    """Test points sorted by size of attribute fails"""
+    with pytest.raises(AssertionError, match="Markersize not based on"):
+        pt_geo_plot_bad.assert_collection_sorted_by_markersize(pd_gdf, "attr")
+        plt.close('all')
+
+
+def test_points_grouped_by_type(pt_geo_plot, pd_gdf):
+    """Tests that points grouped by type passes"""
+    pt_geo_plot.assert_points_grouped_by_type(pd_gdf, "attr")
+    plt.close('all')
+
+
+def test_points_grouped_by_type_fail(pt_geo_plot_bad, pd_gdf):
+    """Tests that points grouped by type passes"""
+    with pytest.raises(AssertionError, match="Point attributes not accurate"):
+        pt_geo_plot_bad.assert_points_grouped_by_type(pd_gdf, "attr")
+        plt.close('all')
+
+
+def test_point_geometry_pass(point_geo_plot, pd_gdf):
+    """Check that the point geometry test recognizes correct points."""
+    point_geo_plot.assert_points(points_expected=pd_gdf)
+    plt.close('all')
+
+
+def test_point_geometry_fail(point_geo_plot, bad_pd_gdf):
+    """Check that the point geometry test recognizes incorrect points."""
+    with pytest.raises(AssertionError, match="Incorrect Point Data"):
+        point_geo_plot.assert_points(points_expected=bad_pd_gdf)
+        plt.close('all')
+
+
+def test_assert_point_fails_list(point_geo_plot, pd_gdf):
+    """
+    Check that the point geometry test fails anything that's not a
+    GeoDataFrame
+    """
+    list_geo = [list(pd_gdf.geometry.x), list(pd_gdf.geometry.y)]
+    with pytest.raises(ValueError, match="points_expected is not expected"):
+        point_geo_plot.assert_points(points_expected=list_geo)
+        plt.close('all')
+
+
+def test_get_points(point_geo_plot, pd_gdf):
+    """Tests that get_points returns correct values"""
+    xy_values = point_geo_plot.get_points()
+    assert list(sorted(xy_values.x)) == sorted(list(pd_gdf.geometry.x))
+    assert list(sorted(xy_values.y)) == sorted(list(pd_gdf.geometry.y))
+    plt.close('all')
+
+
+def test_assert_points_custom_message(point_geo_plot, bad_pd_gdf):
+    """Tests that a custom error message is passed."""
+    message = "Test message"
+    with pytest.raises(AssertionError, match="Test message"):
+        point_geo_plot.assert_points(points_expected=bad_pd_gdf, m=message)
+        plt.close('all')
+
+
+def test_wrong_length_points_expected(pt_geo_plot, pd_gdf, bad_pd_gdf):
+    """Tests that error is thrown for incorrect lenght of a gdf"""
+    with pytest.raises(AssertionError, match="points_expected's length does "):
+        pt_geo_plot.assert_points(bad_pd_gdf.append(pd_gdf), "attr")
+        plt.close('all')
