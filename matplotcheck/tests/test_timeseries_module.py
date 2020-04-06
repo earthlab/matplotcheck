39ba37319e53e935ee9523bbeaebedf25c0a7b5b
Leah Wasser <leah.wasser@colorado.edu>
2020-04-06 10:11:18 -0600

    remove extensive docs within test file

--- matplotcheck/tests/test_timeseries_module.py
+++ matplotcheck/tests/test_timeseries_module.py
@@ -26,67 +26,6 @@ def pt_time_line_plt(pd_df_timeseries):
     return TimeSeriesTester(axis)
 
 
-"""
-matplotlib stores datetime data in a very... unique way. It stores everything
-as the number of days since some epoch. If you plot a dataframe containing
-datetime.datetime objects or pandas.Timestamp objects, it will convert it days
-since epoch. Sometimes matplotlib chooses Jan 1, 1970 as the epoch. Other
-times it chooses Jan 1, 0001. If your data contains time data (i.e. higher
-precision than just dates), matplotlib will store it as fractional days since
-epoch, down to millisecond precision (or whatever precision your data is in).
-For datetime data between these epochs, sometimes it will choose to store it as
-negative days since 1970, other times it will store it as positive days since
-the year 0001.
-
-matplotlib DOES provide functions for converting data from this weird format
-back to datetime.datetime or pandas.Timestamp. However, these functions
-always assume that the 1970 epoch was used.
-
-matplotlib's documentation claims that negative values for datetime data are
-not supported, and therefore data representing dates before 1970 are not
-supported. However, matplotlib will happily plot data before 1970 and its
-conversion functions will happily accept negative numbers and try to convert
-them.
-
-As you might imagine, this presents a number of issues for comparing datetime
-data. Most obviously, it gets unreliable when we have to guess which epoch
-matplotlib chose to use. We have tried a few different methods here: different
-ways of converting the data, converting using both epochs and comparing both,
-etc. All of them were pretty messy.
-
-Additionally, there is the issue of floating point truncation error. matplotlib
-stores this data with numpy.float64, which has 52 mantissa bits, or about 15
-base-10 digits of accuracy. Since the number of days since epoch is often in
-the tens-of-thousands, this means that matplotlib may not be able to accurately
-represent data with millisecond precision. (Basically, the datatype isn't able
-to store such a huge number with such small precision.) The actual available
-precision will depend on the dates being used and the epoch matplotlib chooses.
-
-So to solve these problems, we have done two things:
-
-First, we don't bother to try to convert from matplotlib's data ourselves.
-Instead, we require that instructors provide the expected data in matplotlib's
-format when using assert_xydata(). The easiest way for instructors to do this
-is for them to plot the data themselves, create a matplotcheck object from it,
-and then extract the data using get_xy(). One weird quirk is that matplotlib
-seems to consistently choose the same epoch when plotting the same dataset.
-(However, we are unable to predict which epoch this will be for a given
-dataset, and matplotlib's conversion functions don't always choose the same
-epoch as when the data is plotted.) This solves the problem of being able to
-convert the data.
-
-Second, we use numpy.testing.assert_array_max_ulp() for comparing datetime data
-(or any other type of numeric data). This method of comparison ensures that
-floating-point roundoff error does not cause the assertion to erroneously fail.
-However, this cannot prevent truncation error, and therefore cannot prevent a
-loss of precision. Practically, what this means is that assert_xydata() cannot
-tell the difference between times with differences of tens of milliseconds. If
-it can't tell the difference, it will err on the side of passing.
-
-For more info about the issues we've faced with this, take a look at PR #185
-"""
-
-
 def test_assert_xydata_timeseries(pt_time_line_plt):
     """Tests that assert_xydata() correctly passes with matching timeseries
     data."""
