3b4912aea2ab4db3b44c05e96ef7994c4eaf56ef
nkorinek <nathan.korinek@colorado.edu>
2020-04-21 17:02:40 -0600

    Added in formatting changes requested on github

--- examples/plot_vector_testing.py
+++ examples/plot_vector_testing.py
@@ -1,20 +1,20 @@
 """
-Testing Vector Plots Created Using Geopandas with MatPlotCheck
-==============================================================
+Test Plots of Vector Data (Created Using GeoPandas) with Matplotcheck
+=====================================================================
 
-This vignette will show you how to use Matplotcheck to test spatial vector data
-plots created using Geopandas.
+This vignette will show you how to use Matplotcheck to test plots of spatial
+vector data created using GeoPandas.
 
 """
 
 ################################################################################
-# Setup
-# -----
+# Import Packages
+# ---------------
 # To begin, import the libraries that you need.
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from shapely.geometry import Polygon, LineString
 import geopandas as gpd
 from matplotcheck.vector import VectorTester
@@ -22,11 +22,11 @@ from matplotcheck.vector import VectorTester
 ################################################################################
 # Create Geometry Objects
 # -----------------------
-# To run this test, you first must plot some example data. Below are GeoPandas
-# dataframes created to replicate data that may be used in an earth data
-# science exercise. The polygons could be study areas, the lines could be roads
-# and streams near those study areas, and the points could be measurments
-# within the study areas.
+# To run this test, you must first plot some example data. Below are GeoPandas
+# dataframes created to replicate data that may be used in an earth or
+# environmental data science exercise. The polygons could be study areas,
+# while the lines could be roads and streams near those study areas. The points
+# could be measurements within the study areas.
 
 # Create a polygon GeoDataFrame
 coords = [(2, 4), (2, 4.25), (4.25, 4.25), (4.25, 2), (2, 2)]
@@ -47,7 +47,6 @@ multi_line_gdf = gpd.GeoDataFrame(geometry=gpd.GeoSeries(
 multi_line_gdf["attr"] = ["road", "stream"]
 
 # Create a point GeoDataFrame
-
 points = pd.DataFrame(
     {
         "lat": np.array([1, 2, 1, 0, 4]),
@@ -62,7 +61,6 @@ point_gdf = gpd.GeoDataFrame(
 point_gdf["size"] = [100, 100, 300, 300, 500]
 
 # Create symbology dictionary to use in the legend
-
 line_symb = {"road": "black", "stream": "blue"}
 point_symb = {100: "purple", 300: "green", 500: "brown"}
 
@@ -71,8 +69,8 @@ point_symb = {100: "purple", 300: "green", 500: "brown"}
 # ------------------------
 # Above you created several GeoPandas GeoDataFrame objects that you want
 # to plot. To plot these data according to attribute value, you can group
-# the geometry by attributes and plot within a loop. Once you've created
-# your plot, you are ready to test it using MatPlotCheck.
+# the geometry by attributes and plot within a loop. Once you have created your
+# plot, you will be ready to test it using Matplotcheck
 
 # Plot your data
 fig, ax = plt.subplots()
@@ -94,10 +92,11 @@ for ctype, points in point_gdf.groupby('size'):
 ax.legend(title="Legend", loc=(1.1, .1));
 
 ################################################################################
-# Create A MatPlotCheck VectorTester Object
+# Create A Matplotcheck VectorTester Object
 # -----------------------------------------
-# Once you've created your plot, you can create a MatPlotCheck VectorTester
-# object that can be used to test elements in the plot.
+# Once you have created your plot, you can create a Matplotcheck VectorTester
+# object that can be used to test elements in the plot by providing the ax
+# object to the VectorTester function.
 
 vector_test = VectorTester(ax)
 
@@ -112,19 +111,20 @@ vector_test = VectorTester(ax)
 #
 # .. note::
 #   Most tests are created as assert statements. Thus, if a test fails,
-#   matplotcheck will return an error. If the test passes, no message is
+#   Matplotcheck will return an error. If the test passes, no message is
 #   returned.
 
 ################################################################################
-# Test Point Attribute Values and Geometry xy Locations
-# -----------------------------------------------------
+# Test Point Attribute Values and Geometry (x, y Locations)
+# ---------------------------------------------------------
 # You can check that both the position of the points on the plot and the
 # associated point attribute values are
 # accurate using assert_points(), assert_points_grouped_by_type() and
 # assert_collection_sorted_by_markersize().
 #
-# To check the geometry locations, you can call assert_points() and prove the
-# expected points data, which in this case is the point_gdf object.
+# To check the geometry locations, you can call assert_points() and check the
+# plot data against the expected points data, which in this case is the
+# point_gdf object.
 #
 # If the plot uses point markers that are sized by attribute value, you can
 # check that the size of each marker correctly relates to an attribute value by
@@ -143,36 +143,37 @@ vector_test.assert_points_grouped_by_type(point_gdf, "size")
 vector_test.assert_collection_sorted_by_markersize(point_gdf, "size")
 
 ################################################################################
-# Test Line Attribute Values and Coordinate Information (x, y)
-# ------------------------------------------------------------
+# Test Line Attribute Values and Geometry (Coordinate Information)
+# ----------------------------------------------------------------
 # You can also test the position and plot values of line
 # geometries.
 
 # Check line geometry
 vector_test.assert_lines(multi_line_gdf)
 
-# Check lines plotted by type
+# Check lines are plotted by type
 vector_test.assert_lines_grouped_by_type(multi_line_gdf, "attr")
 
 ################################################################################
-# Testing Polygon Geometry
-# ------------------------
-# Currently, MatPlotCheck is unable to check that polygons were plotted by type.
-# Eventually this will be supported. For the time being though, you can still
-# check that polygons are plotted correctly!
+# Testing Polygon Geometries
+# --------------------------
+# Currently, Matplotcheck is unable to check that polygons were plotted by type.
+# Eventually this will be supported. For now, you can check that polygons are
+# plotted correctly!
 
 # Check Polygons
 vector_test.assert_polygons(polygon_gdf)
 
 ################################################################################
-# Access the Axes object in a Jupyter Notebook
-# --------------------------------------------
-# MatPlotCheck can be used to help grade Jupyter Notebooks as well. The main
-# difference is in how you would store the Axes from the plot you are grading.
-# Below is an example of how you could store the Axes of a plot you are hoping
-# to grade in a notebook.
+# Access Axes Objects in a Jupyter Notebook
+# -----------------------------------------
+# Matplotcheck can be used to test plots in Jupyter Notebooks as well. The main
+# difference is how you access the axes objects from the plot that you want to
+# test.
+# Below is an example of how you could access the axes of a plot you want to
+# test in a Jupyter Notebook.
 
-# First, import the Notebook module from MatPlotCheck
+# First, import the Notebook module from Matplotcheck
 import matplotcheck.notebook as nb
 
 # Plot your data
@@ -194,12 +195,12 @@ for ctype, points in point_gdf.groupby('size'):
 # Add a legend
 ax.legend(title="Legend", loc=(1.1, .1));
 
-# HERE'S WHERE YOU STORE THE PLOT!
-# This line at the end of a cell you are expecting a plot in will store any
-# matplotlib plot made in that cell so you can test it at a later time.
+# Here is where you access the axes objects of the plot for testing.
+# You can add the code line below to the end of any plot cell to store all axes
+# objects created by matplotlib in that cell.
 plot_test_hold = nb.convert_axes(plt, which_axes="current")
 
-# This can then be turned into a VectorTester object.
+# This object can then be turned into a VectorTester object.
 vector_test = VectorTester(plot_test_hold)
 
 # Now you can run the tests as you did earlier!
