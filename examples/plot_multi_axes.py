02be25da0dbcec113ea6318c482d11825ca70164
nkorinek <nathan.korinek@colorado.edu>
2020-04-27 17:40:21 -0600

    Updated as per comments on github

--- examples/plot_multi_axes.py
+++ examples/plot_multi_axes.py
@@ -7,17 +7,25 @@ to test Matplotlib objects with more than one axes in Python.
 
 """
 
-###############################################################################
-# Setup
-# -----
-# You will start by importing the required packages and plotting some examples
-# data on more than one axes. Once you have created your plot, you will create
-# a Matplotcheck ``PlotTester`` object by providing each Matplotlib axes object
-# to a unique ``PlotTester`` object.
+################################################################################
+# Import Packages
+# ---------------
+# To begin, import the libraries that you need.
 
 import matplotcheck.base as pt
 import matplotlib.pyplot as plt
 import pandas as pd
+
+###############################################################################
+# Setup
+# -----
+# First you must create some example data to plot over multiple axes. This
+# data could represent many different types of plots, such as plotting multiple
+# satellite bands, plotting NDVI of multiple study sites over time, and much
+# more.
+# Once you have created your plot, you will create a Matplotcheck
+# ``PlotTester`` object by providing each Matplotlib axes object to a unique
+# ``PlotTester`` object.
 
 # Create example data
 line_1 = pd.DataFrame({"Ax 1 X Vals": [(0), (10)], "Ax 1 Y Vals": [(0), (10)]})
@@ -77,17 +85,17 @@ plot_tester_2.assert_xydata(line_2, xcol="Ax 2 X Vals", ycol="Ax 2 Y Vals")
 #   i.e. plot_tester_1 = testing_plot_2_hold[0]
 
 ################################################################################
-# Access the Axes object in a Jupyter Notebook
-# --------------------------------------------
-# MatPlotCheck can be used to help grade Jupyter Notebooks as well. The main
-# difference is in how you would store the Axes from the plot you are grading.
-# Below is an example of how you could store the Axes of a plot you are hoping
-# to grade in a notebook.
+# Access Axes Objects in a Jupyter Notebook
+# -----------------------------------------
+# Matplotcheck can be used to test plots in Jupyter Notebooks as well. The main
+# difference is how you access the axes objects from the plot that you want to
+# test. Below is an example of how you could access the axes of a plot with
+# multiple axes you want to test in a Jupyter Notebook.
 
-# First, import the Notebook module from MatPlotCheck
+# First, import the Notebook module from Matplotcheck
 import matplotcheck.notebook as nb
 
-# Plot the data
+# Plot the example data on two seperate Axes
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 5))
 
 line_1.plot(ax=ax1, x="Ax 1 X Vals", y="Ax 1 Y Vals")
@@ -98,12 +106,16 @@ fig.suptitle("Figure Title")
 ax1.set(title="Axes 1", xlabel = "Ax 1 X Vals", ylabel = "Ax 1 Y Vals")
 ax2.set(title="Axes 2", xlabel = "Ax 2 X Vals", ylabel = "Ax 2 Y Vals")
 
-# HERE'S WHERE YOU STORE THE PLOT!
-# This line at the end of a cell you are expecting a plot in will store any
-# matplotlib plot made in that cell so you can test it at a later time.
+# Here is where you access the axes objects of the plot for testing.
+# You can add the code line below to the end of any plot cell to store all axes
+# objects created by matplotlib in that cell.
+# Note that the ``which_axes`` value is set to "all". This will return all axes
+# when there are more than one used, such as in this plot.
 plot_test_hold = nb.convert_axes(plt, which_axes="all")
 
-# This can then be turned into a PlotTester object.
-line_figure_tests = pt.PlotTester(plot_test_hold)
+# This object can then be turned into a VectorTester object by accessing its
+# indices.
+plot_tester_1 = VectorTester(plot_test_hold[0])
+plot_tester_2 = VectorTester(plot_test_hold[1])
 
 # Now you can run the tests as you did earlier!
