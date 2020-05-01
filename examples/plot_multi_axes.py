3c9bfebb79d53ba661be738e2a332490fab1c377
nkorinek <nathan.korinek@colorado.edu>
2020-04-30 18:12:17 -0600

    Added changes suggested on github

--- examples/plot_multi_axes.py
+++ examples/plot_multi_axes.py
@@ -1,9 +1,9 @@
 """
-Test Plots with Multiple Axes in Matplotcheck
-=============================================
+Test Plots with Multiple Axes (Created Using Matplotlib) in Matplotcheck
+========================================================================
 
-Below you will find some examples of how to use MatPlotCheck
-to test Matplotlib objects with more than one axes in Python.
+Below you will find some examples of how to use Matplotcheck
+to test figures with multiple axes created using Matplotlib.
 
 """
 
@@ -17,21 +17,24 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 ###############################################################################
-# Setup
-# -----
-# First you must create some example data to plot over multiple axes. This
-# data could represent many different types of plots, such as plotting multiple
-# satellite bands, plotting NDVI of multiple study sites over time, and much
-# more.
+# Create Example Data and Plots
+# -----------------------------
+# First, you need some data to plot over multiple axes of a figure. This data
+# could be satellite imagery bands, calculated values of vegetation indices
+# over time for multiple sites, or any other data for which you want to create
+# multiple plots within one figure. In this example, two Pandas dataframes are
+# created to represent different datasets that can be plotted as individual
+# line plots.
+#
 # Once you have created your plot, you will create a Matplotcheck
-# ``PlotTester`` object by providing each Matplotlib axes object to a unique
-# ``PlotTester`` object.
+# ``PlotTester`` object for each Matplotlib axis object using the
+# ``PlotTester()`` function.
 
 # Create example data
 line_1 = pd.DataFrame({"Ax 1 X Vals": [(0), (10)], "Ax 1 Y Vals": [(0), (10)]})
 line_2 = pd.DataFrame({"Ax 2 X Vals": [(10), (0)], "Ax 2 Y Vals": [(0), (10)]})
 
-# Plot the example data on two seperate Axes
+# Plot example data on two separate axes
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 5))
 
 line_1.plot(ax=ax1, x="Ax 1 X Vals", y="Ax 1 Y Vals")
@@ -47,42 +50,31 @@ plot_tester_1 = pt.PlotTester(ax1)
 plot_tester_2 = pt.PlotTester(ax2)
 
 ###############################################################################
-# Test Each Axes
-# --------------
+# Test Individual Axes Using PlotTester Objects
+# ---------------------------------------------
 # Now that you have two ``PlotTester`` objects, one for each axes, you can run
 # tests like you normally would on the plots!
 #
-# As shown below, both axes share the figure title. You can test for the figure
-# title specifically in the ``assert_title_contains()`` function with the
-# ``title_type`` argument. The default for the ``title_type`` argument is to
-# check the figure and the axes title for the expected string, so make sure to
-# specify if you need to!
+# In this example, both axes share the same figure title. You can test for the
+# figure title specifically in the ``assert_title_contains()`` function with
+# the ``title_type`` argument. The default for the ``title_type`` argument is
+# to check both the figure and the axes title for the expected string, so make
+# sure to specify if you need to!
 
-# Testing the axes titles of both axes
+# Test titles of individual axes
 plot_tester_1.assert_title_contains("Axes 1", title_type = "axes")
 
 plot_tester_2.assert_title_contains("Axes 2", title_type = "axes")
 
-# Testing the figure title of both axes (they share the figure title)
+# Test figure title shared by both axes
 plot_tester_1.assert_title_contains("Figure", title_type = "figure")
 
 plot_tester_2.assert_title_contains("Figure", title_type = "figure")
 
-# Testing the x/y data of both axes
+# Test xy data of individual axes
 plot_tester_1.assert_xydata(line_1, xcol="Ax 1 X Vals", ycol="Ax 1 Y Vals")
 
 plot_tester_2.assert_xydata(line_2, xcol="Ax 2 X Vals", ycol="Ax 2 Y Vals")
-
-###############################################################################
-#
-# .. note::
-#   If you are working on tests for jupyter notebooks, you can call the
-#   line below to capture the student cell in a notebook. Then you can
-#   use that object for testing.
-#   testing_plot_2_hold = nb.convert_axes(plt, which_axes="all").
-#   This returns a list that contains a PlotTester for each axes, which you can
-#   access by calling an index on the list,
-#   i.e. plot_tester_1 = testing_plot_2_hold[0]
 
 ################################################################################
 # Access Axes Objects in a Jupyter Notebook
@@ -95,7 +87,7 @@ plot_tester_2.assert_xydata(line_2, xcol="Ax 2 X Vals", ycol="Ax 2 Y Vals")
 # First, import the Notebook module from Matplotcheck
 import matplotcheck.notebook as nb
 
-# Plot the example data on two seperate Axes
+# Plot example data on individual axes
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (10, 5))
 
 line_1.plot(ax=ax1, x="Ax 1 X Vals", y="Ax 1 Y Vals")
@@ -110,11 +102,11 @@ ax2.set(title="Axes 2", xlabel = "Ax 2 X Vals", ylabel = "Ax 2 Y Vals")
 # You can add the code line below to the end of any plot cell to store all axes
 # objects created by matplotlib in that cell.
 # Note that the ``which_axes`` value is set to "all". This will return all axes
-# when there are more than one used, such as in this plot.
+# when there is more than one axes, such as in this example figure.
 plot_test_hold = nb.convert_axes(plt, which_axes="all")
 
 # This object can then be turned into a VectorTester object by accessing its
-# indices.
+# indices, such as [0] for the first axes of the figure.
 plot_tester_1 = pt.PlotTester(plot_test_hold[0])
 plot_tester_2 = pt.PlotTester(plot_test_hold[1])
 
