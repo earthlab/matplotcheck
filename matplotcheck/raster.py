1dddb686a6609c58a850688e06e92c78540f01d6
Leah Wasser <leah.wasser@colorado.edu>
2020-05-03 16:30:53 -0600

    flake 8

--- matplotcheck/raster.py
+++ matplotcheck/raster.py
@@ -56,6 +56,43 @@ class RasterTester(VectorTester):
                 cb[0].vmax == crange[1]
             ), "Colorbar maximum is not expected value:{0}".format(crange[1])
 
+    def get_legend_labels(self):
+        """Return labels from legend in a list
+
+        Returns
+        -------
+        labels: List
+            List of labels found in the legend of a raster plot.
+        """
+
+        # Retrieve legend
+        legends = self.get_legends()
+        # TODO add better error message --  make this a try except as
+        #  get
+        #  legends should return an error if no legends exist
+        assert legends, "No legend displayed"
+
+        # Get each patch stored in the legends object
+        patches = [leg.get_patches() for leg in legends]
+
+        # Grab rgb, alpha color and associated label for each patch
+        # TODO: this is a nested list because patches is returned as a list
+        #  above. could the patches object every have more than one sublist?
+        # TODO because we are making this power case here we need to ensure
+        #  the expected labels list are also lower case. then we need tests
+        #  for upper and lower case labels in expected labels and in the
+        #  plot legend to ensure this works. current it fails if upper case
+        #  expected labels are provided but lowercase is in the legend.
+        # to simplify will
+        label_dict = {}
+
+        # Iterate through each patch (legend box) and grab label and facecolor
+        for a_patch in patches[0]:
+            label = a_patch.get_label().lower()
+            label_dict[label] = {"color": a_patch.get_facecolor()}
+
+        return label_dict
+
     def _check_label(self, labels, expected_labels):
         """Helper function for assert_legend_labels
         Tests each label in the legend to see if the text in expected labels
@@ -64,6 +101,7 @@ class RasterTester(VectorTester):
         Parameters
         ----------
         # TODO: update all parameters and associated parameter description
+        # input -- dictionary now for labels object
         labels: string from legend to see if it contains an option in
             expected_labels
         expected_labels: list of lists
@@ -88,18 +126,60 @@ class RasterTester(VectorTester):
         #     if label_option == label:
         #         return label_option
 
-        label_check = {}
+        label_check = labels.copy()
 
         # Iteratively test each label found in the plot legend to see if it is
         # in the list of expected labels
-        for i, label in enumerate(labels):
-            test_output = any(
-                label in expected_label
+        # Implementing dictionaries here!
+        for i, a_label in enumerate(labels.keys()):
+            # print(a_label)
+            # for expected_label in expected_labels[i]:
+            #     test = a_label in expected_label
+            #     print(a_label, expected_label)
+            #     print(test)
+
+            label_check[a_label]["match"] = any(
+                a_label in expected_label
                 for expected_label in expected_labels[i]
             )
-            label_check[label] = test_output
+
+            # test2 = [a_label in expected_label
+            # for expected_label in expected_labels[i]]
+            # any(test2)
+            # print(expected_label)
+
+        # for i, label in enumerate(labels):
+        #     test_output = any(
+        #         label in expected_label
+        #         for expected_label in expected_labels[i]
+        #     )
+        #     label_check[label] = test_output
 
         return label_check
+
+    def get_plot_image(self):
+        """Returns images stored on the Axes object as a list of numpy arrays.
+
+        Returns
+        -------
+        im_data: List
+            Numpy array of images stored on Axes object.
+        """
+        im_data = []
+        if self.ax.get_images():
+            im = self.ax.get_images()[0]
+            im_data = im.get_array()
+            im_cmap = im.get_cmap()
+
+        # TODO make this a better test (Try / except??) / return more
+        #  expressive error
+        assert list(im_data), "No Image Displayed"
+
+        # If image array has 3 dims (e.g. rgb image), remove alpha channel
+        if len(im_data.shape) == 3:
+            im_data = im_data[:, :, :3]
+
+        return (im_data, im_cmap)
 
     def assert_raster_legend_labels(self, im_expected, expected_labels):
         """Asserts legend correctly describes classified image on Axes ax,
@@ -130,7 +210,7 @@ class RasterTester(VectorTester):
         # return an error
 
         # Retrieve image array
-        im_data = self.get_plot_image()
+        im_data, im_cmap = self.get_plot_image()
 
         # TODO: We shouldn't need these tests because they happen in
         #  get_plot_image already. But we should improve the output message in
@@ -140,6 +220,7 @@ class RasterTester(VectorTester):
         labels = self.get_legend_labels()
 
         # TODO: i think this should be a try, catch / return value error
+        # this still works as a dictionary as there is 3 keys
         assert len(labels) == len(expected_labels), (
             "Number of label options provided doesn't match the number of"
             " labels found in the image."
@@ -149,7 +230,7 @@ class RasterTester(VectorTester):
         #  better if it returns a dictionary with the key being each
         #  label being tested and the value being a boolean (True if there
         #  is a match, False if there is no match)
-
+        # TODO: UPDATE CKECK LABEL to take input dictionary rather than list
         labels_dict = self._check_label(labels, expected_labels)
 
         # labels_check = [
@@ -157,16 +238,15 @@ class RasterTester(VectorTester):
         #     for i, label in enumerate(labels)
         # ]
 
-        # TODO: this now becomes a bit more tricky but we need to 1) test that
-        # each label is true - if one is false, then return useful message
-        # stating which label is incorrect.
+        # TODO: fix the dict comprehension below to grab the correct key for
+        #  true / false
 
         # Pull out any labels that failed the above test for final printing
         # below
         bad_labels = {
             key: labels_dict[key]
             for key in labels_dict
-            if not labels_dict[key]
+            if not labels_dict[key]["match"]
         }
 
         # TODO: raise assertion error (value error?) and print out a list of
@@ -188,8 +268,38 @@ class RasterTester(VectorTester):
 
         # TODO: once we get the above working, let's then add another layer
         #  where we grab the RGB values and also add that to the dictionary
+        # in the above we have the color and the label. now we need another
+        # dictionary that has the array value and map that to color.
+
         # At that point we can test whether the colors in the plot array, map
         # to the legend patch colors and in turn the expected image
+
+        # Get image  -- this can be a helper...
+        # TODO: remember how cmaps map to data in a np array. i believe we
+        #  can pull from the earthpy legend function to help with this.
+        # We will need to know whether the vmin and vmax are modified in the
+        # plot i think as well... this could get tricky...
+
+        # BEGIN WIP
+        # Essential what this should do is grab the colors used in the plot
+        # for each unique array value. NOTE that we may have to consider both
+        # continuous and non continuous data here (so arrays with 123,
+        # 012 or 0,4,7 as examples) We will need tests for all cases.
+
+        # im_class_dict = {}
+        for val in np.unique(im_data):
+            print(val)
+            # We may need to handle a list different from an existin gcmap
+            # cmap_type = im_cmap.name
+            # im_class_dict[val] = legend_dict[im_cmap(im.norm(val))]
+
+        # im_data_labels = [
+        #     [im_class_dict[val] for val in row] for row in im_data.data
+        # ]
+        # im_expected_labels = [
+        #     [all_label_options[val][0] for val in row] for row in im_expected
+        # ]
+        # END WIP
 
         # Check that expected and actual arrays data match up
         assert np.array_equal(
@@ -200,49 +310,6 @@ class RasterTester(VectorTester):
         #  supported in non-PROJ4 emulation mode - where is this coming from?
 
         # IMAGE TESTS/HELPER FUNCTIONS
-
-    def get_legend_labels(self):
-        """Return labels from legend in a list
-
-        Returns
-        -------
-        labels: List
-            List of labels found in the legend of a raster plot.
-        """
-
-        # Retrieve legend
-        legends = self.get_legends()
-        assert legends, "No legend displayed"
-
-        # Get each patch stored in the legends object
-        patches = [leg.get_patches() for leg in legends]
-
-        # Go through each patch to retrieve the labels from the legend
-        return [
-            label.get_label().lower()
-            for sublist in patches
-            for label in sublist
-        ]
-
-    def get_plot_image(self):
-        """Returns images stored on the Axes object as a list of numpy arrays.
-
-        Returns
-        -------
-        im_data: List
-            Numpy array of images stored on Axes object.
-        """
-        im_data = []
-        if self.ax.get_images():
-            im_data = self.ax.get_images()[0].get_array()
-
-        assert list(im_data), "No Image Displayed"
-
-        # If image array has 3 dims (e.g. rgb image), remove alpha channel
-        if len(im_data.shape) == 3:
-            im_data = im_data[:, :, :3]
-
-        return im_data
 
     def assert_image(
         self, im_expected, im_classified=False, m="Incorrect Image Displayed"
@@ -264,6 +331,9 @@ class RasterTester(VectorTester):
         ----------
         Nothing (if checks pass) or raises error
         """
+        # TODO this should be able to call the get_image helper above rather
+        #  than recreating get image.
+
         im_data = []
         if self.ax.get_images():
             im_data = self.ax.get_images()[0].get_array()
