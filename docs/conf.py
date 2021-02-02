b7fa8687e66373dac556cb17d07b78a3e31afa5f
nkorinek <nako1890@colorado.edu>
2021-02-02 11:13:59 -0700

    Bump version: 0.1.2 → 0.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ copyright = "{year}, Earth Lab Contributors, Leah Wasser".format(
 author = "Leah Wasser"
 
 # The short X.Y version
-version = "0.1.2"
+version = "0.1.3"
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
